173a5b8fd3376f4f44d7a57b0ab77b83b8116749
yukminnie <dadiaomin@126.com>
2021-05-12 20:48:49 +0800

    add electron-reload

--- main.js
+++ main.js
@@ -1,14 +1,25 @@
 // app 是管理应用程序生命周期用的
 // BrowserWindow 是创建还有控制应用窗口用的 ..
 const {app, BrowserWindow } = require('electron')
+const path = require('path')
+
+require('electron-reload')(__dirname, {
+    electron: path.join(__dirname, 'node_modules', '.bin', 'electron')
+})
 
 // 暂时设置成全局变量，防止垃圾回收
 let win
 
 // 定义事件
 const createWindow = () => {
-    win = new BrowserWindow()
-    win.loadURL('https://funnie.cc')
+    win = new BrowserWindow({
+        width: 960,
+        height: 600,
+        minWidth: 830,
+        minHeight: 500,
+        backgroundColor: '#f8f8f8'
+    })
+    win.loadURL(`file://${__dirname}/main.html`)
 }
 
 // 绑定事件
