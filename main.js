9f05962828eb37755e253a843ac699950e459011
yukminnie <dadiaomin@126.com>
2021-05-12 21:29:36 +0800

    mac and win about window-closed different

--- main.js
+++ main.js
@@ -20,7 +20,26 @@ const createWindow = () => {
         backgroundColor: '#f8f8f8'
     })
     win.loadURL(`file://${__dirname}/main.html`)
+
+    // 手动关闭窗口, 应用会做什么，接下面 all-closed 和 activate
+    win.on('closed', () => {
+        win = null
+    })
 }
 
 // 绑定事件
 app.on('ready', createWindow);
+
+// 不是 mac 平台就完全退出
+app.on('window-all-closed', () => {
+    if(process.platform !== "darwin") {
+        app.quit()
+    }
+})
+
+// mac 平台可以重新激活
+app.on('activate', () => {
+    if(win = null) {
+        createWindow()
+    }
+})
